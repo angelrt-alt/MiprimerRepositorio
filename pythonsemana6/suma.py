83c69f6c3d6014e6fcea7339644d01617c64683c
angelrt-alt <angelrt.contraataque@gmail.com>
2026-04-16 16:26:55 -0600

    clase semana 6

---
+++ pythonsemana6/suma.py
@@ -0,0 +1,45 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Victor"
+print(nombre)
+nombre = 40
+print(nombre)
+# binario = 0
+# binario = 1
+
+
+# string = letras
+# objeto = 012345
+
+# iinmutabilidad:
+# pero siempre deben conservar su tipo
+
+
+## direcion # 0x
+## tipo => numerico o string (booleando, objeto)
+## tipo numero => perpara a la memoria
+## para guardar un tipo de dato string exa
+
+## textos largos vamos a utilizar las comillas triples
+texto_largo = """ lorem ipsum dolor dit amet, consectetur adipiscing elit.     string literal is unterminated
+sed do eiusmod tempr incididunt ut labore et dolore magna aliqua."""
+print(texto_largo)
+## las comillas triples se usan para el texto largos
+## mantienen el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("el resultado de la suma es:,", resultado)
+
+
+resultado = numero1 + numero2
+print("el resultado de la suma es:,", resultado)
+
+
+resultado = numero1 + numero2
+print("el resultado de la suma es:,", resultado)
